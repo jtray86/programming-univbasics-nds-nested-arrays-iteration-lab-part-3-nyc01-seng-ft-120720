9d9659a3695192115de8ef75dc4f25b25777471c
Jen Tracy <jenniferetracy@gmail.com>
2020-11-16 15:43:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row< src.count do
     element=0 
     while element <src[row].count do
-      if src[row][element].class == string
+      if src[row][element].class == ""
         sentence << src[row][element]
       end
     element+=1
